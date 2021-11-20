ffe4a196757379c64f56e87be4ae043802302bf7
Brenda <brenda_pereiraornelas@hotmail.com>
2021-11-20 13:34:19 -0300

    feature: add user service

---
+++ src/main/java/com/panacademy/squad7/bluebank/services/UsersServiceImpl.java
@@ -0,0 +1,52 @@
+package com.panacademy.squad7.bluebank.services;
+
+import com.panacademy.squad7.bluebank.domain.models.User;
+import com.panacademy.squad7.bluebank.domain.repositories.UsersRepository;
+import com.panacademy.squad7.bluebank.exceptions.ContentNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+
+    private final UsersRepository UsersRepository;
+
+    public UsersServiceImpl(UsersRepository UsersRepository) {
+        this.UsersRepository = UsersRepository;
+    }
+
+    @Override
+    public User create(User User) {
+        return UsersRepository.save(User);
+    }
+
+
+    @Override
+    public User update(User User, Long id) {
+        return UsersRepository.findById(id).map(a -> {
+            User.setId(id);
+            return UsersRepository.save(User);
+        }).orElseThrow(() -> new ContentNotFoundException("User not found with id " + id));
+    }
+
+    @Override
+    public void delete(Long id) {
+        if (UsersRepository.existsById(id)) {
+            UsersRepository.deleteById(id);
+        } else {
+            throw new ContentNotFoundException("User not found with id " + id);
+        }
+    }
+
+    @Override
+    public User findById(Long id) {
+        return UsersRepository.findById(id)
+                .orElseThrow(() -> new ContentNotFoundException("User not found with id " + id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return UsersRepository.findAll();
+    }
+}
