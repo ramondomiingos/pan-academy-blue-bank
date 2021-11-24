554b57c763a66dd88f57b81351c8e35b68f7dc66
William Jesus <williamjesusti@gmail.com>
2021-11-23 22:50:34 -0300

    chore: refactor code of swagger

--- src/main/java/com/panacademy/squad7/bluebank/exceptions/BlueBankRunTimeExceptionHandler.java
+++ src/main/java/com/panacademy/squad7/bluebank/exceptions/BlueBankRunTimeExceptionHandler.java
@@ -4,6 +4,7 @@ import com.panacademy.squad7.bluebank.exceptions.dtos.ApiExceptionsDTO;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,14 +33,20 @@ public class BlueBankRunTimeExceptionHandler extends ResponseEntityExceptionHand
   @ExceptionHandler(value = { ContentNotFoundException.class })
   public ResponseEntity<Object> handleContentNotFoundException(ContentNotFoundException exception) {
     logger.error("Content Not Found Exception: " + exception.getMessage());
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiExceptionsDTO(exception));
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiExceptionsDTO(exception));
   }
 
   @Override
   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
       HttpStatus status, WebRequest request) {
     List<String> errors = ex.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList());
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiExceptionsDTO("Invalid Parameters: ", errors));
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiExceptionsDTO("Invalid Parameters", errors));
+  }
+
+  @ExceptionHandler(value = { DataIntegrityViolationException.class} )
+  public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
+    logger.error("Data Integrity Violation Exception: " + exception.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiExceptionsDTO(exception));
   }
 
 }
--- src/main/java/com/panacademy/squad7/bluebank/web/controllers/AddressesController.java
+++ src/main/java/com/panacademy/squad7/bluebank/web/controllers/AddressesController.java
@@ -4,16 +4,23 @@ import com.panacademy.squad7.bluebank.services.AddressesService;
 import com.panacademy.squad7.bluebank.shared.converters.AddressConverter;
 import com.panacademy.squad7.bluebank.web.dtos.request.AddressRequest;
 import com.panacademy.squad7.bluebank.web.dtos.response.AddressResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/addresses")
+@Tag(name = "Addresses", description = "endpoint for all address requests")
 public class AddressesController {
 
     @Autowired
@@ -23,13 +30,19 @@ public class AddressesController {
     private AddressConverter addressConverter;
 
     @PostMapping
-    public ResponseEntity<AddressResponse> create(@RequestBody AddressRequest address) {
+    @Operation(summary = "Add a new address", responses = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "Address Not Found", content = @Content())
+    })
+    public ResponseEntity<AddressResponse> create(@Valid @RequestBody AddressRequest address) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(addressConverter.toResponse(addressesService.create(addressConverter.toModel(address))));
     }
 
     @GetMapping
+    @Operation(summary = "Find all addresses", responses = {@ApiResponse(responseCode = "200", description = "Success")})
     public ResponseEntity<List<AddressResponse>> getAll() {
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -37,6 +50,11 @@ public class AddressesController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find address by ID", responses = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "Address Not Found", content = @Content())
+    }, parameters = {@Parameter(description = "Id of the address for search")})
     public ResponseEntity<AddressResponse> getById(@PathVariable Long id) {
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -45,13 +63,22 @@ public class AddressesController {
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<AddressResponse> update(@PathVariable Long id, @RequestBody AddressRequest address) {
+    @Operation(summary = "Update an address", responses = {
+            @ApiResponse(responseCode = "201", description = "Updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid Input", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "Address Not Found", content = @Content())
+    }, parameters = {@Parameter(description = "Id of the address for search")})
+    public ResponseEntity<AddressResponse> update(@PathVariable Long id, @Valid @RequestBody AddressRequest address) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(addressConverter.toResponse(addressesService.update(addressConverter.toModel(address), id)));
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Delete an address", responses = {
+            @ApiResponse(responseCode = "204", description = "Deleted", content = @Content()),
+            @ApiResponse(responseCode = "404", description = "Address Not Found", content = @Content())
+    }, parameters = {@Parameter(description = "Id of the address for search")})
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         addressesService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
