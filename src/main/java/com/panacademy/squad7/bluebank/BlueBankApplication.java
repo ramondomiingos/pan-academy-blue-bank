f1a5723c5d8617da227f6150125a6093a2381bf3
WilliamJesusDev <williamjesusti@gmail.com>
2021-11-26 14:50:49 -0300

    chore: enabling cors

--- src/main/java/com/panacademy/squad7/bluebank/BlueBankApplication.java
+++ src/main/java/com/panacademy/squad7/bluebank/BlueBankApplication.java
@@ -2,12 +2,26 @@ package com.panacademy.squad7.bluebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BlueBankApplication {
+public class BlueBankApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlueBankApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
