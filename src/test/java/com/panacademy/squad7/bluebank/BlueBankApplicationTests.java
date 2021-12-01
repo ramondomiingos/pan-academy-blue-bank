3edb68f14675ada73e075cbac40848bb7bccbfbf
WilliamJesusDev <williamjesusti@gmail.com>
2021-12-01 14:42:55 -0300

    bugfix: fix tests environment

--- src/test/java/com/panacademy/squad7/bluebank/BlueBankApplicationTests.java
+++ src/test/java/com/panacademy/squad7/bluebank/BlueBankApplicationTests.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
 @ActiveProfiles("test")
-@SpringBootTest()
 class BlueBankApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void contextLoads() {}
 
 }
--- src/test/java/com/panacademy/squad7/bluebank/controllers/ClientsControllerTest.java
+++ src/test/java/com/panacademy/squad7/bluebank/controllers/ClientsControllerTest.java
@@ -1,5 +1,8 @@
 package com.panacademy.squad7.bluebank.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.panacademy.squad7.bluebank.domain.enums.ClientType;
+import com.panacademy.squad7.bluebank.web.dtos.request.ClientRequest;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -7,39 +10,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.panacademy.squad7.bluebank.domain.enums.ClientType;
-import com.panacademy.squad7.bluebank.web.dtos.request.ClientRequest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.time.LocalDate;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ActiveProfiles("test")
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@WithMockUser(username = "test", authorities = {"ROLE_USER", "ROLE_ADMIN"})
 public class ClientsControllerTest {
     @Autowired
     private MockMvc mockMvc;
-  
+
     @Autowired
     private ObjectMapper objectMapper;
-    
-    private ClientRequest clientRequest = new ClientRequest();
-    
+
+    private ClientRequest clientRequest;
+
     @BeforeAll
     private void setUp() throws Exception {
+        clientRequest = new ClientRequest();
         clientRequest.setName("Fulano");
         clientRequest.setLastname("de Tal");
-        clientRequest.setBirthDate(LocalDate.of(1990,12,18));
+        clientRequest.setBirthDate(LocalDate.of(1990, 12, 18));
         clientRequest.setMotherName("Fulano");
         clientRequest.setEmail("fulano@email.com");
         clientRequest.setPassword("fulano");
@@ -48,7 +47,7 @@ public class ClientsControllerTest {
         clientRequest.setType(ClientType.NP);
         clientRequest.setRegistration("38441875880");
     }
- 
+
     @Test
     public void whenPostClients_thenStatus201() throws Exception {
         mockMvc.perform(post("/clients")
@@ -57,9 +56,10 @@ public class ClientsControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-    }    
-    
+    }
+
     @Test
+
     public void whenGetClients_thenStatus200() throws Exception {
         mockMvc.perform(get("/clients")
                         .contentType(MediaType.APPLICATION_JSON))
@@ -67,8 +67,9 @@ public class ClientsControllerTest {
                 .andExpect(content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
-    
+
     @Test
+    //@WithMockUser(username = "test", authorities = {"ROLE_USER", "ROLE_ADMIN"})
     public void whenGetClientById_thenStatus200() throws Exception {
         mockMvc.perform(get("/clients/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -78,8 +79,9 @@ public class ClientsControllerTest {
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.name").value(clientRequest.getName()));
     }
-    
+
     @Test
+    //@WithMockUser(username = "test", authorities = {"ROLE_USER", "ROLE_ADMIN"})
     public void whenGetClientById_thenStatus404() throws Exception {
         mockMvc.perform(get("/clients/{id}", 100)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -89,11 +91,12 @@ public class ClientsControllerTest {
                 .andExpect(jsonPath("$.message").exists())
                 .andExpect(jsonPath("$.message").value("client not found with id 100"));
     }
-    
+
     @Test
+    //@WithMockUser(username = "test", authorities = {"ROLE_USER", "ROLE_ADMIN"})
     public void whenPutClients_thenStatus201() throws Exception {
-        clientRequest.setName("Sicrano"); 
-    	mockMvc.perform(put("/clients/{id}", 1)
+        clientRequest.setName("Sicrano");
+        mockMvc.perform(put("/clients/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(clientRequest)))
                 .andExpect(status().isCreated())
